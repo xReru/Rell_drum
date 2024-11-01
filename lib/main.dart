3fa88e8eab3d645ee72b19220f00be3e6193cb9c
Janrell Quiaroro <git.xreru@gmail.com>
2024-11-01 23:17:44 +0800

    hehe

--- lib/main.dart
+++ lib/main.dart
@@ -88,22 +88,6 @@ class _MainAppState extends State<MainApp> {
                     Builder(
                       builder: (BuildContext context) {
                         return IconButton(
-                          icon: const Icon(Icons.import_export_rounded,
-                              color: Color(0xffEEC640)),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const AboutLayout(),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                    Builder(
-                      builder: (BuildContext context) {
-                        return IconButton(
                           icon:
                               const Icon(Icons.info, color: Color(0xffEEC640)),
                           onPressed: () {
